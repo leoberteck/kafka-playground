62a50d2cab447698c9bce7c89ff08bbfb047a3e1
leonardo.stabile <leonardo.stabile@solinftec.com.br>
2019-11-06 09:21:25 -0300

    delelte k-monitoramento

--- console-producer.sh
+++ console-producer.sh
@@ -6,10 +6,13 @@ confluent-5.3.1/bin/kafka-avro-console-producer \
   --property parse.key=true \
   --property key.separator=,\
   --property key.schema='{"type":"record","name":"pgsinkdemo_key","fields":[{"name":"id","type":"int"}]}' \
-  --property value.schema='["null",{"type":"record","name":"pgsinkdemo","fields":[{"name":"id","type":"int"},{"name":"product","type":"string"},{"name":"quantity","type":"int"},{"name":"price","type":"float"}]}]'
+  --property value.schema='{"type":"record","name":"pgsinkdemo","fields":[{"name":"id","type":"int"},{"name":"product","type":"string"},{"name":"quantity","type":"int"},{"name":"price","type":"float"}]}'
 
 # { "id": 1 },{ "id": 1, "product": "foo", "quantity": 100, "price": 10 }
 # { "id": 2 },{ "id": 2, "product": "bolacha", "quantity": 10, "price": 20 }
 # { "id": 3 },{ "id": 3, "product": "farofa", "quantity": 2, "price": 13 }
 # { "id": 4 },{ "id": 4, "product": "macarrao", "quantity": 32, "price": 15 }
 # { "id": 5 },{ "id": 5, "product": "arroz", "quantity": 5, "price": 3 }
+
+# CREATE STREAM ks_orders (id INT, product VARCHAR, quantity INT, price INT) WITH (KAFKA_TOPIC='orders', VALUE_FORMAT='AVRO');
+# CREATE TABLE kt_orders (id INT, product VARCHAR, quantity INT, price INT) WITH (KAFKA_TOPIC='orders', VALUE_FORMAT='AVRO', KEY = 'id');
--- k-monitoramento/modelo/src/main/java/com/solinftec/k/modelo/Equipamento.java
+++
@@ -1,5 +0,0 @@
-package com.solinftec.k.modelo;
-
-public class Equipamento {
-  
-}
